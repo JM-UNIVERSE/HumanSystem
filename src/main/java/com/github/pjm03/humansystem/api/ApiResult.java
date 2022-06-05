b2a40985332c5179c984cb4af04801774d6487de
jm-universe <inomq5129@naver.com>
2022-06-06 02:24:58 +0900

    Swagger 제거

--- src/main/java/com/github/pjm03/humansystem/api/ApiResult.java
+++ src/main/java/com/github/pjm03/humansystem/api/ApiResult.java
@@ -1,6 +1,5 @@
 package com.github.pjm03.humansystem.api;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,15 +7,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public class ApiResult<T> {
-    @Schema(description = "Http 응답 상태", example = "OK")
     private final HttpStatus status;
-    @Schema(description = "Http 응답 상태 코드", example = "200")
     private final int code;
-    @Schema(description = "결과값 반환 시각", example = "1025098671")
     private final long timestamp;
-    @Schema(description = "에러 메시지", example = "에러 메시지")
     private final String errMsg;
-    @Schema(description = "반환 결과값")
     private final T data;
 
     public static <K> ApiResult<K> success(K data) {
